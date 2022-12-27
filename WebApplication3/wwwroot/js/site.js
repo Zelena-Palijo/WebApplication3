898d7d0f1c305615b0b1fb8a1fc14ee7eb3f9a57
Christopher Quattrocchi <zpali853@gmail.com>
2022-12-26 22:30:16 -0800

    adds js

--- WebApplication3/wwwroot/js/site.js
+++ WebApplication3/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+
+function hover() {
+    document.getElementById("over").style.color = "orange";
+
+}
+
+function hoverout() {
+    document.getElementById("over").style.color = "black";
+
+}
